47b92f07ed2a61338ffb4d948f8e0720ecea1c86
Kevin Parks <parkske@onid.oregonstate.edu>
2015-08-06 11:27:40 -0700

    Creates Stormy weather app. Most of Model in place.

---
+++ Stormy/app/src/main/java/com/kparks/stormy/AlertDialogFragment.java
@@ -0,0 +1,22 @@
+package com.kparks.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Kevin Parks on 8/6/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button_text, null);
+        return builder.create();
+    }
+}
---
+++ Stormy/app/src/main/java/com/kparks/stormy/CurrentWeather.java
@@ -0,0 +1,109 @@
+package com.kparks.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Kevin Parks on 8/6/2015.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private String mTimeZone;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        return formatter.format(new Date(mTime * 1000));
+    }
+
+    public int getIconId() {
+        //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        int iconId = R.mipmap.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        } else if (mIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        } else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        } else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        } else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        } else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        } else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        } else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        } else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        } else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return iconId;
+    }
+}
